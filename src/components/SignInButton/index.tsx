16e8888c83fb6d58145ddbffeecc9986215b94e0
Natanael <nata.mw@hotmail.com>
2021-03-27 00:21:42 -0300

    :sparkles: feat: created sessions user in stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,11 +7,10 @@ import styles from './styles.module.scss';
 
 export function SignInButton() {
   const [session] = useSession();
-  console.log(session);
   return session ? (
     <button type="button" className={styles.SignInButton}>
       <FaGithub color="#84d361" />
-      Sign in with Github
+      {session.user.name}
       <FiX
         color="#737388"
         className={styles.closeIcon}
