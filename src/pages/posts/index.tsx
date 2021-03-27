01bbab8dd22d0883ae6ec03c09d37818af08adf3
Natanael <nata.mw@hotmail.com>
2021-03-27 04:44:00 -0300

    :sparkles: feat: created posts pages

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,73 @@
+import Head from 'next/head';
+
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews </title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>26 de março 2021</time>
+            <strong>Create postss</strong>
+            <p>
+              create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd
+              asdas create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd
+              adasd asdas create possadi sjdasod sdjsiajdasdai isajdiasda
+              dasijdiajd adasd asdas create possadi sjdasod sdjsiajdasdai
+              isajdiasda dasijdiajd adasd asdas create possadi sjdasod
+              sdjsiajdasdai isajdiasda dasijdiajd adasd asdas create possadi
+              sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd asdas create
+              possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd asdas
+              create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd
+              asdas create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd
+              adasd asdas
+            </p>
+          </a>
+          <a href="#">
+            <time>26 de março 2021</time>
+            <strong>Create postss</strong>
+            <p>
+              create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd
+              asdas create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd
+              adasd asdas create possadi sjdasod sdjsiajdasdai isajdiasda
+              dasijdiajd adasd asdas create possadi sjdasod sdjsiajdasdai
+              isajdiasda dasijdiajd adasd asdas create possadi sjdasod
+              sdjsiajdasdai isajdiasda dasijdiajd adasd asdas create possadi
+              sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd asdas
+            </p>
+            <p>
+              create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd
+              asdas create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd
+              adasd asdas create possadi sjdasod sdjsiajdasdai isajdiasda
+              dasijdiajd adasd asdas create possadi sjdasod sdjsiajdasdai
+              isajdiasda dasijdiajd adasd asdas create possadi sjdasod
+              sdjsiajdasdai isajdiasda dasijdiajd adasd asdas create possadi
+              sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd asdas
+            </p>
+          </a>
+          <a href="#">
+            <time>26 de março 2021</time>
+            <strong>Create postss</strong>
+            <p>
+              create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd
+              asdas create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd
+              adasd asdas create possadi sjdasod sdjsiajdasdai isajdiasda
+              dasijdiajd adasd asdas create possadi sjdasod sdjsiajdasdai
+              isajdiasda dasijdiajd adasd asdas create possadi sjdasod
+              sdjsiajdasdai isajdiasda dasijdiajd adasd asdas create possadi
+              sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd asdas create
+              possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd asdas
+              create possadi sjdasod sdjsiajdasdai isajdiasda dasijdiajd adasd
+              asdas
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
